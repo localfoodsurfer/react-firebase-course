88e214fd5ca55fe0bdc2d81cea8d9c9ad90c59df
practice-firebase-react-course <vidula@localfoodsurfer.com>
2023-08-03 00:05:23 +0530

    firebase.js updated to include the firebase console api key. auth.js updated to include the emailpassword signin and signin with google methods

--- src/config/firebase.js
+++ src/config/firebase.js
@@ -1,21 +1,30 @@
-import { initializeApp } from "firebase/app";
-import { getAuth, GoogleAuthProvider } from "firebase/auth";
-import { getFirestore } from "firebase/firestore";
-import { getStorage } from "firebase/storage";
 
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+//import { getAnalytics } from "firebase/analytics";
+import { getAuth } from "firebase/auth";
+import {getFirestore} from 'firebase/firestore';
+import { getStorage } from "firebase/storage";
+import { getauth, GoogleAuthProvider } from "firebase/auth";
+
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyAlztMMVXKXA5oSVpwe0XthJ6TWNe31BSo",
-  authDomain: "fir-course-beba9.firebaseapp.com",
-  projectId: "fir-course-beba9",
-  storageBucket: "fir-course-beba9.appspot.com",
-  messagingSenderId: "236316955671",
-  appId: "1:236316955671:web:2b18d92e1b6644fae3f852",
-  measurementId: "G-HENJ7D82KH",
+  apiKey: "AIzaSyAHL6GfDuYdiOj1t9KYRgETJmPmJ1VMCpQ",
+  authDomain: "react-firebase-13926.firebaseapp.com",
+  projectId: "react-firebase-13926",
+  storageBucket: "react-firebase-13926.appspot.com",
+  messagingSenderId: "502810231143",
+  appId: "1:502810231143:web:92a032b24e0d815c196fc2",
+  measurementId: "G-YC1EKB7SPF"
 };
 
+// Initialize Firebase
 const app = initializeApp(firebaseConfig);
-export const auth = getAuth(app);
-export const googleProvider = new GoogleAuthProvider();
-
+export const auth = getAuth (app);
 export const db = getFirestore(app);
 export const storage = getStorage(app);
+export const googleProvider = new GoogleAuthProvider();
